9112d7787c236a8fb07d2df426ff0851e8bd776b
miyass <exe.miyakura0629@gmail.com>
2018-07-15 00:52:04 +0900

    to get an information from RESAS API and create Line by LineRenderer

---
+++ Assets/Scripts/APIRequestScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class APIRequestScript : MonoBehaviour {
+
+	private string API_KEY = "Ew8FnGHoOEvZTZD05u6Ye8n0N8xFvzOd4uYXE7Js";
+	private string parameter = "year=2015&prefCode=1&countryCode=304&purpose=1";
+	private string url = "https://opendata.resas-portal.go.jp/api/v1/tourism/foreigners/forTo?";
+
+	private int totalVisitor;
+
+	//jsonのキーとclass変数の名前は一致させる
+	[System.Serializable]
+	public class Response
+	{
+		public string message;
+		public Result result;
+	}
+
+	[System.Serializable]
+	public class Result
+	{
+		public int year;
+		public string countryName;
+		public string prefName;
+		public Changes[] changes;
+
+	}
+
+	[System.Serializable]
+	public class Changes
+	{
+		public string prefName;
+		public string rank;
+		public Data[] data;
+	}
+
+	[System.Serializable]
+	public class Data 
+	{
+		public int year;
+		public int quarter;
+		public int value;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GetInfo());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator GetInfo() {
+		UnityWebRequest request = UnityWebRequest.Get (url + parameter);
+		request.SetRequestHeader("Content-Type", "application/json");
+		request.SetRequestHeader("Accepted", "application/json");
+		request.SetRequestHeader("X-API-KEY", API_KEY);
+
+		yield return request.Send();
+
+		if (request.isError) {
+			Debug.Log(request.error);
+		} else {
+			string jsonText = request.downloadHandler.text;
+			Debug.Log(jsonText);
+			Response res = JsonUtility.FromJson<Response> (jsonText);
+			//前年度の部分まで取得される 0~3:前年度1~4quater 4~7:取得年度1~4quater
+			for (int i = 4; i <= 7; i++){
+				int visitor = res.result.changes [0].data[i].value;
+				totalVisitor += visitor;
+			}
+			Debug.Log (totalVisitor);
+		}
+	}
+}
---
+++ Assets/Scripts/LineDrawScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawScript : MonoBehaviour {
+
+	public GameObject LineRendererObject;
+	public GameObject earth;
+	GameObject japan;
+	GameObject america;
+
+	[Range(0, 1)]
+	float t;
+	public Transform top;
+	LineRenderer line;
+
+	// Use this for initialization
+	void Start () {
+		japan = earth.transform.FindChild ("Japan").gameObject;
+		america = earth.transform.FindChild ("America").gameObject;
+		t = 0.1f;
+		line = LineRendererObject.GetComponent<LineRenderer> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		LineRendererObject.transform.position = BezierCurve (
+			america.transform.position,
+			japan.transform.position,
+			top.transform.position,
+			t
+		);
+		OnLineDraw ();
+	}
+
+	public static Vector3 BezierCurve(Vector3 pt1, Vector3 pt2, Vector3 ctrlPt, float t) {
+		if (t > 1.0f)
+			t = 1.0f;
+
+		Vector3 result = new Vector3();
+		float cmp = 1.0f - t;
+		result.x = cmp * cmp * pt1.x + 2 * cmp * t * ctrlPt.x + t * t * pt2.x;
+		result.y = cmp * cmp * pt1.y + 2 * cmp * t * ctrlPt.y + t * t * pt2.y;
+		result.z = cmp * cmp * pt1.z + 2 * cmp * t * ctrlPt.z + t * t * pt2.z;
+		return result;	
+	}
+
+	public void OnLineDraw() {
+		var posList = new List<Vector3>();
+		posList.Add(america.transform.position);
+		float length = 0f;
+		while (length < 1f)
+		{
+			length += 0.1f;
+			posList.Add(
+				BezierCurve(
+					america.transform.position,
+					japan.transform.position,
+					top.position,
+					length
+				)
+			);
+		}
+		line.material = new Material (Shader.Find("Particles/Additive"));
+		line.SetColors (Color.green, Color.blue);
+		line.startWidth = 0.8f;
+		line.endWidth = 0.8f;
+		line.numPositions = posList.Count;
+		line.SetPositions(posList.ToArray());
+	}
+}
